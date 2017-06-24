2dc57013dd05a11c0633369bb8a3f3973b247a17
Nikola Lukic <luknik94@gmail.com>
2017-06-24 18:07:02 +0200

    Initial model.

---
+++ pravlapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PravlappConfig(AppConfig):
+    name = 'pravlapp'
---
+++ pravlapp/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(unique=True, max_length=256)
+    password = models.CharField(max_length=256)
+    email = models.EmailField()
+
+
+class Device(models.Model):
+    serial_number = models.CharField(unique=True, max_length=256)
+    name = models.CharField(max_length=256)
+    heartbeat = models.IntegerField()
+    active = models.BooleanField()
+    user = models.ForeignKey(User)
+
+
+class Reading(models.Model):
+    type = models.CharField(max_length=256)
+    current_value = models.FloatField()
+    last_update = models.DateTimeField()
+    sensor = models.ForeignKey(Device)
+
+
+class Rule(models.Model):
+    name = models.CharField(max_length=256)
+    active = models.BooleanField()
+    definition = models.TextField()
+    user = models.ForeignKey(User)
+    devices = models.ManyToManyField(Device)
+
+
+class Message(models.Model):
+    timestamp = models.DateTimeField()
+    rule = models.ForeignKey(Rule)
+    user = models.ForeignKey(User)
+
+
+class FirebaseToken(models.Model):
+    token = models.CharField(max_length=1000)
+    user = models.ForeignKey(User)
+
