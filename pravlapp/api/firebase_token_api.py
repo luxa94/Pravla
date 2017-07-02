905e31b37cc20435ffd2dcefba1e7b3965beea67
Nikola Lukić <luknik94@gmail.com>
2017-07-02 17:05:09 +0200

    Firebase token creation done properly.

--- pravlapp/api/firebase_token_api.py
+++ pravlapp/api/firebase_token_api.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 
-from pravlapp.models import FirebaseToken
 from pravlapp.serializers import FirebaseTokenSerializer
 from pravlapp.util.decorators import Authenticated
 
@@ -12,7 +11,7 @@ class FirebaseTokenDetail(APIView):
     def post(self, request, user):
         serializer = FirebaseTokenSerializer(data=request.data)
         if serializer.is_valid():
-            FirebaseToken.objects.create(user=user, **serializer.data)
+            serializer.save(user=user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
