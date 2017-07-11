b97be12cfebee03dbe42cad48debec91c5101d9c
Jasna Stanković <jasnaa994@gmail.com>
2017-07-11 17:40:38 +0200

    Device and rule update on frontend. (#3)

--- pravlapp/static/rule/rule.service.js
+++ pravlapp/static/rule/rule.service.js
@@ -11,9 +11,15 @@
     function ruleService($http) {
 
         return {
+            find: find,
             findAll: findAll,
-            add : add
+            add : add,
+            edit: edit
         };
+
+        function find(id) {
+            return $http.get(`rules/${id}`);
+        }
 
         function findAll() {
             return $http.get('rules');
@@ -21,6 +27,10 @@
 
         function add(rule) {
             return $http.post('rules', rule);
+        }
+
+        function edit(rule) {
+            return $http.put(`rules/${rule.id}`, rule);
         }
     }
 })(angular);
